09a5b57fac0b3da5d5928253eac70707c3d4eefd
loc <loctruong96@gmail.com>
2020-05-25 00:16:13 -0700

    experiment1.py script

--- experiment1.py
+++ experiment1.py
@@ -1,25 +1,14 @@
 import os
 import time
 
-from output_test import read_proper_fasta, mutation_and_rigidity_test
-mutation_residues = 'SYLVTSV'
-chain = 'A'
+start_index = 2837
+end_index = 2843
+chain = 'G'
 sequences = ['2DM8', 'MBDLG', '3RL7']
 start = time.time()
+total = start
 for sequence in sequences:
     print(f'processing {sequence}')
-    fasta_file = f'promute/{sequence}.fasta'
-    assert os.path.exists(fasta_file), f'{fasta_file} does not exists.'
-    fasta_file_content = read_proper_fasta(fasta_file)
-    assert mutation_residues in fasta_file_content, f'{mutation_residues} does not exists in {sequence}.'
-    start_index = fasta_file_content.index(mutation_residues)
-    end_index = fasta_file_content.index(mutation_residues)+(len(mutation_residues)-1)
-    assert start_index <= len(fasta_file_content)-1
-    assert end_index <= len(fasta_file_content) - 1
-    test = ""
-    for i in range(start_index, end_index+1):
-        test += fasta_file_content[i]
-    assert test == mutation_residues, 'start index and end index mismatched.'
     output_folder_name = f'{sequence}{chain}{start_index}:{end_index}_out/'
     assert os.path.exists('multiMutant.sh'), 'multiMutant script is missing.'
     if os.path.exists(output_folder_name):
@@ -28,12 +17,14 @@ for sequence in sequences:
     print(f'execute: ./multiMutant.sh {sequence} {chain} {start_index}:{end_index} -em')
     os.system(f'./multiMutant.sh {sequence} {chain} {start_index}:{end_index} -em')
     print(f'command took {round(time.time()-start)} seconds.')
+    start = time.time()
     assert os.path.exists(output_folder_name), f'output folder for {sequence} is missing.'
     os.chdir(output_folder_name)
     assert os.path.exists('sequentialPipelineInvocation.sh'), 'sequentialPipelineInvocation script is missing.'
     print('execute: ./sequentialPipelineInvocation.sh')
     os.system(f'./sequentialPipelineInvocation.sh')
     print(f'command took {round(time.time() - start)} seconds.')
+    start = time.time()
     os.chdir('..')
 
-print(f'total time taken {round(time.time()-start)} seconds.')
+print(f'total time taken {round(time.time()-total)} seconds.')
