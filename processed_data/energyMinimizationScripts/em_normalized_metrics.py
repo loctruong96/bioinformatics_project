da5bf8ca60054f59a7b59cd3112b29ae9e0f60d7
mcculls5 <mcculls5@wwu.edu>
2020-06-07 20:24:39 -0700

    Computed normalized EM Metrics for all data

--- processed_data/energyMinimizationScripts/em_normalized_metrics.py
+++ processed_data/energyMinimizationScripts/em_normalized_metrics.py
@@ -1,12 +1,57 @@
 import pandas as pd
+import numpy as np
+from scipy import stats
 
 groups = {
     "mbDLG_1": ["2DM8", "3RL7", "MBDLG"],
-    "mbDLG_2": ["2BYG", "MBDLG2HB008", "MBDLG2HB010"]
+    "mbDLG_2": ["2BYG", "MBDLG2HB008", "MBDLG2HB010"],
     "mbDLG_3": ["2HE2", "MBDLG3"]
 }
 
 for group in groups.keys():
-    for pdb in group:
-        dir = group+"/"+pdbID+"/
-        with open(+pdbID+"_normalized_em_metrics.csv") as output:
+    for pdbID in groups[group]:
+        for i in range(2):
+            dir = "../"+group+"/"+pdbID+"/"
+            baselineFile = dir+"base_"+pdbID+"_em_metrics"
+            metricsFile = dir+pdbID+"_em_metrics"
+            outputFile = dir+pdbID+"_normalized_em_metrics"
+            if(i == 1):
+                metricsFile += "_2"
+                outputFile += "_2"
+            
+            baseline = pd.read_csv(baselineFile+".csv")
+
+            allMetrics = pd.read_csv(metricsFile+".csv")
+            
+
+            mt10_95 = allMetrics.STEPS_MT10_95
+            wt10_95 = allMetrics.STEPS_WT10_95
+
+            norm_mt10_95 = mt10_95 - baseline.STEPS_MT10_95[0]
+            norm_wt10_95 = wt10_95 - baseline.STEPS_WT10_95[0]
+
+            mtZ = stats.zscore(mt10_95)
+            wtZ = stats.zscore(wt10_95)
+
+            nmtZ = stats.zscore(norm_mt10_95)
+            nwtZ = stats.zscore(norm_wt10_95)
+
+            d = {
+                "PDBID": allMetrics.PDBID,
+                "MUTATION": allMetrics.MUTATION,
+                "MT10_95": mt10_95,
+                "Z_MT10_95": mtZ,
+                "D_MT10_95": norm_mt10_95,
+                "Z_D_MT10_95": nmtZ,
+                "WT10_95": wt10_95,
+                "Z_WT10_95": wtZ,
+                "D_WT10_95": norm_wt10_95,
+                "Z_D_WT10_95": nwtZ,
+                "BELOW_WT": allMetrics.STEPS_WT_100
+            }
+
+            df = pd.DataFrame(data=d)
+
+            df.to_csv(outputFile+".csv", index=False, 
+            columns= ["PDBID", "MUTATION", "MT10_95", "Z_MT10_95", "D_MT10_95", "Z_D_MT10_95", "WT10_95", "Z_WT10_95", "D_WT10_95", "Z_D_WT10_95", "BELOW_WT"])
+            
---
+++ processed_data/mbDLG_2/emAnalysisScripts/base_compute_em_metrics.py
@@ -0,0 +1,171 @@
+import csv
+
+pdbList = ["2BYG", "MBDLG2HB008", "MBDLG2HB010"]
+
+pdbBase = {
+    #Step0, Step10, Step500
+    "2BYG": [404799.5093, 357807.9499, 14764.8495],
+    "MBDLG2HB008": [151026.6855, 122965.1753, -1454.3302],
+    "MBDLG2HB010": [124172.9443, 95512.1972, -744.2225]
+}
+
+columns = ["PDBID", 
+           "MUTATION", 
+           "INITIAL", 
+           "FINAL", 
+           "STEP10"
+            ]
+        #    "STEPS_25", 
+        #    "STEPS_50", 
+        #    "STEPS_75", 
+        #    "STEPS_W25", 
+        #    "STEPS_W50", 
+        #    "STEPS_W75",
+        #    "STEPS_W100"]
+
+percentThresholds = ["25", "50", "75", "90", "95"]
+decimalThresholds = [0.25, 0.5, 0.75, 0.9, 0.95]
+
+for i in range(len(percentThresholds)):
+    columns.append("STEPS_MT_"+percentThresholds[i])
+
+for i in range(len(percentThresholds)):
+    columns.append("STEPS_MT10_"+percentThresholds[i])
+
+for i in range(len(percentThresholds)):
+    columns.append("STEPS_WT_"+percentThresholds[i])
+
+for i in range(len(percentThresholds)):
+    columns.append("STEPS_WT10_"+percentThresholds[i])
+columns.append("STEPS_WT_100")
+
+for pdb in pdbList:
+    print("../"+pdb+"/base_"+pdb+"_raw_em_step_data.csv")
+    mFile = open("../"+pdb+"/base_"+pdb+"_raw_em_step_data.csv", "r")
+    mData = csv.reader(mFile)
+        
+    out = open("../"+pdb+"/base_"+pdb+"_em_metrics.csv","w" ,newline='')
+    writer = csv.writer(out, delimiter=",")
+    writer.writerow(columns)
+    
+    diff = float(pdbBase[pdb][0]) - float(pdbBase[pdb][2])
+    diff10 = float(pdbBase[pdb][1]) - float(pdbBase[pdb][2])
+    wtThresholds =[]
+    wt10Thresholds =[]
+    for i in range(len(percentThresholds)):
+        wtThresholds.append(float(pdbBase[pdb][0]) - (decimalThresholds[i]*diff))
+        wt10Thresholds.append(float(pdbBase[pdb][1]) - (decimalThresholds[i]*diff10))
+
+    
+    
+
+    data = {}
+    
+    #skip header
+    next(mData)
+    
+    for row in mData:
+        if row[1] not in data.keys():
+            data[row[1]] = {
+                "INITIAL": 0,
+                "FINAL": 0,
+                "STEP10": 0,
+                "STEPS_WT_100": 1000
+            }
+            for i in range(len(percentThresholds)):
+                # #These need to be initialized above the max stepcount (500)
+                data[row[1]]["STEPS_MT_"+percentThresholds[i]] = 1000
+                data[row[1]]["STEPS_MT10_"+percentThresholds[i]] = 1000
+                data[row[1]]["STEPS_WT_"+percentThresholds[i]] = 1000
+                data[row[1]]["STEPS_WT10_"+percentThresholds[i]] = 1000
+                
+                # data[row[1]]["MT_"+percentThresholds[i]] = 0
+                # data[row[1]]["MT10_"+percentThresholds[i]] = 0
+                
+            data[row[1]]["PDBID"] = pdb
+            data[row[1]]["MUTATION"] = row[1]
+        
+        if int(row[2]) == 0:
+            data[row[1]]["INITIAL"] = float(row[3])
+        if int(row[2]) == 10:
+            data[row[1]]["STEP10"] = float(row[3])
+        if int(row[2]) == 500:
+            data[row[1]]["FINAL"] = float(row[3])
+            
+
+
+    for mutant in data.keys():
+        diff = data[mutant]["INITIAL"] - data[mutant]["FINAL"]
+        diff10 = data[mutant]["STEP10"] - data[mutant]["FINAL"]
+        for i in range(len(percentThresholds)): 
+            data[mutant]["MT_"+percentThresholds[i]] = data[mutant]["INITIAL"] - (decimalThresholds[i] * diff)
+            data[mutant]["MT10_"+percentThresholds[i]] = data[mutant]["STEP10"] - (decimalThresholds[i] * diff10)
+
+
+    mFile.seek(0)
+
+    next(mData)
+
+    for row in mData:  
+        for i in range(len(percentThresholds)):
+            if float(row[2]) < data[row[1]]["STEPS_MT_"+percentThresholds[i]] and float(row[3]) < float(data[row[1]]["MT_"+percentThresholds[i]]):
+                data[row[1]]["STEPS_MT_"+percentThresholds[i]] = int(row[2])
+                
+            if float(row[2]) < data[row[1]]["STEPS_MT10_"+percentThresholds[i]] and float(row[3]) < float(data[row[1]]["MT10_"+percentThresholds[i]]):
+                data[row[1]]["STEPS_MT10_"+percentThresholds[i]] = int(row[2])
+                
+            if float(row[2]) < data[row[1]]["STEPS_WT_"+percentThresholds[i]] and float(row[3]) < float(wtThresholds[i]):
+                data[row[1]]["STEPS_WT_"+percentThresholds[i]] = int(row[2])
+                
+            if float(row[2]) < data[row[1]]["STEPS_WT10_"+percentThresholds[i]] and float(row[3]) < float(wt10Thresholds[i]):
+                data[row[1]]["STEPS_WT10_"+percentThresholds[i]] = int(row[2])
+                
+                
+        if float(row[2]) < data[row[1]]["STEPS_WT_100"] and float(row[3]) < float(pdbBase[pdb][2]):
+            data[row[1]]["STEPS_WT_100"] = int(row[2])    
+            
+    for mutant in data:
+        
+        for i in range(len(percentThresholds)):
+            if data[mutant]["STEPS_MT_"+percentThresholds[i]] == 1000:
+                data[mutant]["STEPS_MT_"+percentThresholds[i]] = -1
+                
+            if data[mutant]["STEPS_MT10_"+percentThresholds[i]] == 1000:
+                data[mutant]["STEPS_MT10_"+percentThresholds[i]] = -1
+                
+            if data[mutant]["STEPS_WT_"+percentThresholds[i]] == 1000:
+                data[mutant]["STEPS_WT_"+percentThresholds[i]] = -1
+                
+            if data[mutant]["STEPS_WT10_"+percentThresholds[i]] == 1000:
+                data[mutant]["STEPS_WT10_"+percentThresholds[i]] = -1
+                
+                
+        if data[mutant]["STEPS_WT_100"] == 1000:
+            data[mutant]["STEPS_WT_100"]= -1
+                 
+        row =[
+            data[mutant]["PDBID"], 
+            data[mutant]["MUTATION"], 
+            data[mutant]["INITIAL"], 
+            data[mutant]["FINAL"],
+            data[mutant]["STEP10"]
+        ]
+        
+        for i in range(len(percentThresholds)):
+            row.append( data[mutant]["STEPS_MT_"+percentThresholds[i]])
+
+        for i in range(len(percentThresholds)):
+            row.append( data[mutant]["STEPS_MT10_"+percentThresholds[i]])
+
+        for i in range(len(percentThresholds)):
+            row.append( data[mutant]["STEPS_WT_"+percentThresholds[i]])
+            
+        for i in range(len(percentThresholds)):
+            row.append( data[mutant]["STEPS_WT10_"+percentThresholds[i]])
+           
+        row.append(data[mutant]["STEPS_WT_100"])
+         
+        writer.writerow(row)
+        
+    out.close()
+    mFile.close()
